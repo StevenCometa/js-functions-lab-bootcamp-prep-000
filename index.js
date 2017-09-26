989e38341ff3f1b00bad681e6b32641fdafeb168
Steven Cometa <sdcometa@gmail.com>
2017-09-26 07:09:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 }
 
-function happyHoldayTo(holiday,name) {
+function happyHolidayTo(holiday,name) {
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown(days,name) {
+function holidayCountdown(days,holiday) {
   return `It's ${days} days until ${holiday}!`
 }
